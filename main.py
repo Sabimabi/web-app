6f2d54357d0aaf8f4a1e64de9cc9af7c81503927
Sabimabi <rukulo.0303@gmail.com>
2025-03-02 12:20:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+user_preferences = {}  
+recipes = []  
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        return redirect(url_for("preferences"))
+    else:
+        return render_template('index.html')  
+
+@app.route("/preferences", methods=["GET", "POST"])
+def preferences():
+    if request.method == "POST":
+        user_preferences["dietary_restrictions"] = request.form.get("dietary_restrictions", "")
+        user_preferences["favorite_cuisine"] = request.form.get("favorite_cuisine", "")
+        user_preferences["meals_per_week"] = request.form.get("meals_per_week", "")
+
+        return redirect(url_for("recipe"))
+    else:
+        return render_template('preferences.html', user_preferences=user_preferences)
+
+@app.route("/recipe", methods=["GET", "POST"])
+def recipe():
+    if request.method == "POST":
+        recipe_name = request.form.get("recipe_name")
+        ingredients = request.form.get("ingredients")
+        recipes.append({"name": recipe_name, "ingredients": ingredients})
+
+        return redirect(url_for("recipeplan"))
+    else:
+        return render_template('recipe.html')
+
+@app.route("/recipeplan")
+def recipeplan():
+    if not recipes:
+        message = "No recipes have been entered. Please add recipes to see your plan."
+        return render_template('recipeplan.html', message=message)
+    else:
+        return render_template('recipeplan.html', user_preferences=user_preferences, recipes=recipes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
